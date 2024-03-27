fbd06ae0bc9ae1bb93eba9182175db82d7e34f07
Lica Simon <116165856+simonlica@users.noreply.github.com>
2024-03-27 10:55:21 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,16 @@
+import os
+
+#print("Olá mundo")
+
+print("O 3DS é a turma numero 1 do Sagrada")
+
+turma="2DS"
+pos=2
+
+print("o",turma,"é a turma numero", pos,"do Sagrada")
+
+print(type(turma))
+
+print(type(pos))
+
+os.system("cls")
